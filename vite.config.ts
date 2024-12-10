d4bff148fffd8bd58f0f05a2d0067ff9a61e264d
danfordChris <jurvisdanford329@gmail.com>
2024-12-11 01:14:02 +0300

    Changes to be committed: 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Specify the output directory to be 'build'
+  },
   server: {
     hmr: {
       overlay: false,
